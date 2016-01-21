e10616840604fa56319ba14e8483c23ff92642b1
Willian Silva <willian.silva@concretesolutions.com.br>
2016-01-21 15:29:54 -0200

    Refactored code using functions.
    Refactored code using functions.

--- imc.rb
+++ imc.rb
@@ -3,28 +3,36 @@
 # encoding: utf-8
 # vim: ft=ruby 
 # Program: imc 
-# Description: Calcule IMC
-#
+# Description: Calculo do IMC
 # Author: Willian Martins <willian.martins@msn.com>
-#
 #####################################################################
 
-puts "\n\n\nEste programa fara o calculo do seu IMC. \n"
+
+def calc_imc(a, p)
+	aqd = (a * a) / 10000
+	imc = p / aqd
+	r_imc(imc)
+end 
+
+def r_imc(i)
+	print "\nSeu IMC é #{i.round(2)} - "
+	puts "Voce esta muito abaixo do peso" if i < 17
+	puts "Voce esta abaixo do peso" if (i > 17) && (i < 18.49)
+	puts "Voce esta no peso ideal" if (i >18.50) && (i < 24.99)
+	puts "Voce esta acima do peso" if (i > 25) && (i < 29.99)
+	puts "Voce esta com obesidade I" if (i > 30) && (i < 34.99)
+	puts "Voce esta com obesidade II (Severa)" if (i > 35) && (i < 39.99)
+	puts "Voce esta com obesidade III (Morbida)" if i >40
+end
+
+puts "\nEste programa fara o calculo do seu IMC. \n"
+
 puts "\nInforme sua altura em centimetros (Ex: 1,80 --> 180)"
 altura = gets.chomp.to_f
-altura = (altura * altura) / 10000
+puts "Altura não informada/Incorreta" if altura == nil || altura == 0
 
 puts "\nInforme seu peso"
 peso = gets.chomp.to_f
+puts "Peso nao informado/Incorreto" if peso == nil || peso == 0
 
-imc =  peso / altura
-puts "\nSeu IMC é #{imc.round(2)} \n \n"
-
-puts "Voce esta muito abaixo do peso" if imc < 17
-puts "Voce esta abaixo do peso" if (imc > 17) && (imc < 18.49)
-puts "Voce esta no peso ideal" if (imc >18.50) && (imc < 24.99)
-puts "Voce esta acima do peso" if (imc > 25) && (imc < 29.99)
-puts "Voce esta com obesidade I" if (imc > 30) && (imc < 34.99)
-puts "Voce esta com obesidade II (Severa)" if (imc > 35) && (imc < 39.99)
-puts "Voce esta com obesidade III (Morbida)" if imc >40
-puts "\n \n \n "
+calc_imc(altura, peso)
